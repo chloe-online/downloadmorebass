aca5cc4ee282237d3c610e72c07ffffd428871a0
chloe-online <chloestraus99@gmail.com>
2026-07-09 20:56:57 -0400

    clean up repo

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import Song from "./Song.svelte";
+  import Song from "./components/Song.svelte";
   import { fetchTracks } from "./lib/api";
   import type { Track } from "../shared/types";
 
